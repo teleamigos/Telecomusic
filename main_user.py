8a898ce6a74f0954a8d9686ecf7ba1b0b2141165
OrlandoTele96 <orlandoglezmusic@gmail.com>
2019-06-04 17:45:34 -0500

    Boton para descargar programado

--- main_user.py
+++ main_user.py
@@ -1,16 +1,44 @@
 
 from tkinter import *
+import socket
 
+
+"""---------------------------------------------------------------------
+--------------------------SendInfo-----------------------------------"""
+
+def SendInfo(Information):
+    #Realiza la conexion con el servidor para enviar informacion.
+    IP='127.0.0.1'
+    PORT=3000
+    print("This is your petition : ")
+    print(Information)
+
+
+
+"""---------------------------------------------------------------------
+--------------------------DownloadButton-----------------------------"""
+
+def DownloadButton():
+    #### Manda  una solicitud en forma de string al servvidor sobre la cancion
+    ## que se quiere
+    txt=entry.get()
+    SendInfo(txt)
+    root.destroy()
+
+
+"""--------------------------------------------------------------------
+--------------------------Ventana principal-------------------------"""
 root=Tk()
 root.title("Telecomusic")
 root.configure(background="pale goldenrod")
-Text_Entry=Entry(root)
-Text_Entry.pack()
-Text_Entry.focus_set()
-def DownloadButton():
-    print(Text_Entry.get())
-
+entry=Entry(root)
+entry.pack()
+entry.focus_set()
 Dbutton=Button(root,text="Download",width=10,command=DownloadButton)
 Dbutton.pack()
 
 root.mainloop()
+
+"""Se cierra la ventana y continua con la conexion"""
+
+print("Ya se mando")
