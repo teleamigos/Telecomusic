69b1f959cb844bdf6e67ded1630eee3f549d05c1
OrlandoTele96 <orlandoglezmusic@gmail.com>
2019-06-04 22:11:42 -0500

    Busca archivos

--- main_server.py
+++ main_server.py
@@ -2,13 +2,30 @@ import socket
 import wave
 
 
-"""---------------------------------------------------------------------
---------------------------List of songs------------------------------"""
-
 
 
 """---------------------------------------------------------------------
---------------------------Server-------------------------------------"""
+--------------------------findFile-------------------------------------"""
+def findFile(lista,nombre):
+    if nombre in lista:
+        return 1
+    else:
+        return 0
+
+"""---------------------------------------------------------------------
+--------------------------main-------------------------------------"""
+"""List of songs"""
+
+lista = ['LaFemme',
+        'PinkFloyd',
+        'ACDC',
+        'LedZeppelin',
+        'TheDoors',
+        'TheRollingStones',
+        'Queen',
+        'Soko']
+
+"""Server"""
 IP='127.0.0.1'
 PORT=3000
 
@@ -23,11 +40,29 @@ with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
             #print(data)
             if not data:
                 break
+"""Searching the song"""
+isInList=findFile(lista,data)
 
-
-"""---------------------------------------------------------------------
---------------------------Searching the song-------------------------"""
-
+if isInList == 1:
+    print('Se envia la cancion al cliente')
+    if nombre == 'Soko':
+        print('funcion para elegir Soko.wav')
+    if nombre == 'LaFemme':
+        print('funcion para elegir LaFemme.wav')
+    if nombre == 'PinkFloyd':
+        print('funcion para elegir PinkFloyd.wav')
+    if nombre == 'ACDC':
+        print('funcion para elegir ACDC.wav')
+    if nombre == 'LedZeppelin':
+        print('funcion para elegir LedZeppelin.wav')
+    if nombre == 'TheDoors':
+        print('funcion para elegir TheDoors.wav')
+    if nombre == 'TheRollingStones':
+        print('funcion para elegir TheRollingStones.wav')
+    if nombre == 'Queen':
+        print('funcion para elegir Queen.wav')
+else:
+    print('Se devuelve mensaje de error al cliente')
 """---------------------------------------------------------------------
 --------------------------Opening a audio file-----------------------"""
 
